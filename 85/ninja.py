d0d12bbd0bdff8820a20b2c1a07f7d2955deef19
morphik83 <balazy.maciek@gmail.com>
2020-02-07 23:42:01 +0100

    resolved.

--- 85/ninja.py
+++ 85/ninja.py
@@ -5,18 +5,45 @@ BELTS = dict(zip(scores, ranks))
 
 class NinjaBelt:
 
-    def __init__(self, score=0):
-        self._score = score
-        self._last_earned_belt = None
+  def __init__(self, score=0):
+    self._score = score
+    self._last_earned_belt = None
 
-    def _get_belt(self, new_score):
-        """Might be a useful helper."""
-        pass
+  def _get_belt(self, new_score):
+    is_new_belt_earned = False
+    if new_score < 10:
+      self._last_earned_belt = ''
+    elif new_score > 1000:
+      self._last_earned_belt = 'red'
+    else:
+      for index, value in enumerate(scores):
+        if new_score < value:
+          belt = BELTS.get(scores[index - 1]).title()
+          if belt != self._last_earned_belt:
+            self._last_earned_belt = belt
+            is_new_belt_earned = True
+          break
+        elif new_score == value:
+          belt = BELTS.get(scores[index])
+          self._last_earned_belt = belt.title()
+          is_new_belt_earned = True
+          break
+    return is_new_belt_earned
 
-    def _get_score(self):
-        pass
+  @property
+  def score(self):
+    return self._score
 
-    def _set_score(self, new_score):
-        pass
+  @score.setter
+  def score(self, new_score):
+    if isinstance(new_score, int) and new_score >= self._score:
+      self._score = new_score
+      if not self._get_belt(new_score):
+        print(f'Set new score to {self._score}')
+      else:
+        print(
+            f'Congrats, you earned {self._score} points obtaining the PyBites Ninja {self._last_earned_belt} Belt')
+    else:
+      raise ValueError
 
-    score = property(_get_score, _set_score)
+  # score = property(_get_score, _set_score)
